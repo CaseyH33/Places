47db970b1b6fcaf5f94dca149d68299126406109
Casey Heitz & Mitch Rivet <student@epicodus.com>
2015-08-05 14:28:56 -0700

    stores places

---
+++ app/app.php
@@ -0,0 +1,35 @@
+<?php
+  require_once __DIR__."/../vendor/autoload.php";
+  require_once __DIR__."/../src/Places.php";
+
+  session_start();
+
+  if (empty($_SESSION['list_of_places'])) {
+    $_SESSION['list_of_places'] = array();
+  }
+
+  $app = new Silex\Application();
+
+  $app->register(new Silex\Provider\TwigServiceProvider(), array (
+    'twig.path' => __DIR__.'/../views'
+  ));
+
+
+
+  $app->get("/", function() use ($app) {
+    return $app['twig']->render('places.twig', array('places' => Places::getAll()));
+  });
+
+  $app->post("/places", function() use ($app) {
+    $place = new Places($_POST['place_name']);
+    $place->save();
+    return $app['twig']->render('create_place.twig', array('newplace' => $place));
+  });
+
+  $app->post("/delete_places", function() use ($app) {
+    Places::deleteAll();
+    return $app['twig']->render('delete_places.twig');
+  });
+
+return $app;
+?>
--- src/Places.php
+++ src/Places.php
@@ -2,21 +2,37 @@
 
     class Places
     {
-      private $city_name
+      private $city_name;
 
       function __construct($city)
       {
           $this->city_name = $city;
+
       }
 
       function getCityName()
         {
-            return $this->$city_name;
+            return $this->city_name;
         }
 
       function setCityName($new_city_name)
       {
             $this->city_name = $new_city_name;
+      }
+
+      function save()
+      {
+        array_push($_SESSION['list_of_places'], $this);
+      }
+
+      static function deleteAll()
+      {
+        $_SESSION['list_of_places'] = array();
+      }
+
+      static function getAll()
+      {
+        return $_SESSION['list_of_places'];
       }
     }
 
